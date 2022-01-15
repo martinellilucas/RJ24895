800b74ecdf7b222fa7eeb36a5dd357480d98daa1
DESKTOP-4N5G45S\lucas <martinellilucas8@gmail.com>
2022-01-15 14:30:06 -0300

    Primera Clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Seccion from "./Seccion.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>HOLA MUNDO !</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
+      <Seccion/>
     </div>
   );
 }
